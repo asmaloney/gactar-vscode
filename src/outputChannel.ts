ff6edce43331a20e643da7f6066bed44c33b4bec
Andy Maloney <asmaloney@gmail.com>
2022-05-29 11:08:55 -0400

    Add some syntax colouring to the output panel

--- src/outputChannel.ts
+++ src/outputChannel.ts
@@ -3,7 +3,10 @@ import * as vscode from 'vscode'
 export let gactarOutputChannel: vscode.OutputChannel
 
 function init(context: vscode.ExtensionContext) {
-  gactarOutputChannel = vscode.window.createOutputChannel('gactar')
+  gactarOutputChannel = vscode.window.createOutputChannel(
+    'gactar',
+    'gactar-output'
+  )
   context.subscriptions.push(gactarOutputChannel)
 }
 
