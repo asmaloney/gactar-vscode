6d355a8c8ae4ebfce385fbfe861a0b98b48012c2
Andy Maloney <asmaloney@gmail.com>
2022-05-30 11:00:56 -0400

    Fix formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,12 +13,10 @@ module.exports = {
   overrides: [
     {
       files: ['*.ts'],
-
       extends: [
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
       ],
-
       parserOptions: {
         project: ['./tsconfig.json'],
       },
